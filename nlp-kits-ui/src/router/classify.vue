3b48fe0d1a7a43667685cf57276523646f20b069
huiwenj <jhwerrr@gmail.com>
2024-09-07 10:58:00 -0700

    update ui

---
+++ nlp-kits-ui/src/router/classify.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>111</h1>
+
+</template>
+
+<style scoped>
+
+</style>
--- nlp-kits-ui/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000 // 可自定义开发服务器的端口
-  }
-})
