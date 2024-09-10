7b823e2bbc2c242626efa2b5628321b001e5b13b
huiwenj <jhwerrr@gmail.com>
2024-09-09 22:50:40 -0700

    update

--- nlp-kits-ui/src/layouts/default.vue
+++ nlp-kits-ui/src/layouts/default.vue
@@ -1,10 +1,12 @@
 <template>
+  <v-theme-provider class = "pa-10" theme = "light" with-background>
   <v-app>
     <v-main>
       <router-view />
     </v-main>
 
   </v-app>
+  </v-theme-provider>
 </template>
 
 <script setup>
